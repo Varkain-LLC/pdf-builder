f5e0cced369eca3afdec83214a0584665dd37476
Elya <elya.manoukian@gmail.com>
2020-07-09 02:52:34 +0400

    Updated converter fully

--- converter/converter.py
+++ converter/converter.py
@@ -1,13 +1,29 @@
+import json
+import os
 import sys
 
 sys.path.append('..')
-
 from tools.html_to_pdf_converter import get_pdf_from_html
 
-if len(sys.argv) != 3:
-    print("usage: converter.py <html_page_sourse> <filename_to_save>")
-    exit()
+from jinja2 import Environment, FileSystemLoader
 
-result = get_pdf_from_html(sys.argv[1], chromedriver='../chromedriver')
-with open(sys.argv[2], 'wb') as file:
+# The part where we get JSON data and create temp HTML file
+# For details read the README file
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+
+template = env.get_template('book.html')
+
+with open('assets/book.json') as book:
+    output = template.render(json_obj=json.loads(str(book.read())))
+
+    # Write HTML String to temp.html
+    with open("temp.html", "w") as file:
+        file.write(output)
+
+# The part where we get the new created temp.html and convert to PDF file
+# For details read the README file
+result = get_pdf_from_html('file://' + os.getcwd() + '/temp.html', chromedriver='../chromedriver')
+with open('assets/book.pdf', 'wb') as file:
     file.write(result)
+    os.remove("temp.html")
