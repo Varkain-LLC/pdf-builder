d75aefc659de23656d8aaa468d2dac9a20912c6f
Elya <elya.manoukian@gmail.com>
2020-07-09 04:07:02 +0400

    separated follders directions in converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -1,11 +1,17 @@
-import json
 import os
+import json
+
 import sys
 
 sys.path.append('..')
-from tools.html_to_pdf_converter import get_pdf_from_html
 
+from tools.html_to_pdf_converter import get_pdf_from_html
 from jinja2 import Environment, FileSystemLoader
+
+# Used folders
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+assetsFolder = os.path.join(BASE_DIR, '../assets/')
+driverFolder = assetsFolder + '/drivers/'
 
 # The part where we get JSON data and create temp HTML file
 # For details read the README file
@@ -14,7 +20,7 @@ env = Environment(loader=file_loader)
 
 template = env.get_template('book.html')
 
-with open('../assets/book.json') as book:
+with open(assetsFolder + 'book.json') as book:
     output = template.render(json_obj=json.loads(str(book.read())))
 
     # Write HTML String to temp.html
@@ -23,7 +29,7 @@ with open('../assets/book.json') as book:
 
 # The part where we get the new created temp.html and convert to PDF file
 # For details read the README file
-result = get_pdf_from_html('file://' + os.getcwd() + '/temp.html', chromedriver='../assets/drivers/chromedriver')
-with open('../assets/book.pdf', 'wb') as file:
+result = get_pdf_from_html('file://' + os.getcwd() + '/temp.html', chromedriver=driverFolder + 'chromedriver')
+with open(assetsFolder + 'book.pdf', 'wb') as file:
     file.write(result)
     os.remove("temp.html")
