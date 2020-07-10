04e4c95bbf38f97ab456c5c59b89352982882c27
Elya <elya.manoukian@gmail.com>
2020-07-10 20:00:42 +0400

    update package name and typo

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='pdf-builder',
-    version='0.0.1',
+    name='PDFbuilder',
+    version='0.0.2',
     author='Elya',
     author_email='elya@saasuma.com',
     url='https://github.com/Varkain-LLC/PDF-builder/',
     packages=['converter'],
     description='HTML to PDF with Python-Jinja',
     long_description=open('README.md').read(),
-    keywords=['pdf-builder'],
+    keywords=['pdf', 'pdf_builder', 'pdf-builder', 'PDFbuilder'],
     install_requires=[
         'selenium',
         'jinja2'
