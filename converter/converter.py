2c1c3f413df88dbd216d8ba982557f5ddcea8948
Elya <elya.manoukian@gmail.com>
2020-07-10 18:11:00 +0400

    Converter as Class to be used in other apps

--- converter/converter.py
+++ converter/converter.py
@@ -1,45 +1,62 @@
 import os
 import json
-
-import sys
+import time
 
 from tools.html_to_pdf_converter import get_pdf_from_html
 from jinja2 import Environment, FileSystemLoader
 
-if len(sys.argv) != 4:
-    print("usage: converter.py <json_source> <html_template_source> <filename_to_save>")
-    exit()
-
 # Used folders
-assetsFolder = os.path.join(os.path.dirname(__file__), '../assets/')
-driverFolder = assetsFolder + '/drivers/'
+assetsFolder = '../assets/'
+driverPath = assetsFolder + '/drivers/chromedriver'
 
 
-# The part where we get JSON data and create temp HTML file
-# For details read the README file
-class HtmlCreator:
-    file_loader = FileSystemLoader('templates')
-    env = Environment(loader=file_loader)
-
-    template = env.get_template(sys.argv[2])
-
-    with open(assetsFolder + sys.argv[1]) as book:
-        output = template.render(json_obj=json.loads(str(book.read())))
-
-        # Write HTML String to temp.html
-        with open("temp.html", "w") as file:
-            file.write(output)
-
-
-# The part where we get the new created temp.html and convert to PDF file
+# The CLASS where we get JSON data and template
+# create temp HTML file
+# convert HTML to PDF
 # For details read the README file
 class HtmlToPdfConverter:
-    result = get_pdf_from_html('file://' + os.getcwd() + '/temp.html', chromedriver=driverFolder + 'chromedriver')
-    with open(assetsFolder + sys.argv[3], 'wb') as file:
-        file.write(result)
-        os.remove("temp.html")
+    def __init__(self, template_vars, html_template):
+        print(f'Initializing {template_vars}, {html_template}')
+        self.template_vars = template_vars
+        self.html_template = html_template
+
+    def create(self, output_file=None):
+        file_loader = FileSystemLoader('templates')
+        env = Environment(loader=file_loader)
+        template = env.get_template(self.html_template)
+        input_file = assetsFolder + self.template_vars
+
+        # Read JSON
+        with open(input_file) as book:
+            print('Rendering template')
+            output = template.render(json_obj=json.loads(str(book.read())))
+            # Write HTML String to temp.html
+            with open("temp.html", "w") as file:
+                file.write(output)
+
+        # Temp to PDF
+        result = get_pdf_from_html(
+            path='file://' + os.getcwd() + '/temp.html',
+            chromedriver=driverPath
+        )
+
+        if not output_file:
+            output_file = str(int(time.time()))  # casting it first to int, in order to get rid of the milliseconds
+
+        output_file = str(assetsFolder) + str(output_file) + '.pdf'
+
+        with open(output_file, 'wb') as file:
+            print('Converting to PDF')
+            file.write(result)
+            os.remove('temp.html')
+            print('PDF file is located at : ' + os.path.abspath(output_file))
 
 
 if __name__ == "__main__":
-    HtmlCreator
-    HtmlToPdfConverter
+    html_to_pdf_obj = HtmlToPdfConverter(
+        template_vars='book.json',
+        html_template='book.html'
+    )
+    html_to_pdf_obj.create(
+        output_file='asdfasdf'
+    )
