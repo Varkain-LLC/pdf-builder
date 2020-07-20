02893d201f6fc31c88c7ed2ae181732c154ae8f5
Elya <elya.manoukian@gmail.com>
2020-07-20 15:30:38 +0400

    removed json_file_path line from get_json function

--- converter/converter.py
+++ converter/converter.py
@@ -25,8 +25,7 @@ class HtmlToPdfConverter:
 
     def get_json(self):
         if self.json_file_path:
-            json_file_path = os.path.join(ASSETS_DIR, self.json_file_path)
-            with open(os.path.abspath(json_file_path)) as json_file:
+            with open(os.path.abspath(os.path.join(ASSETS_DIR, self.json_file_path))) as json_file:
                 return json.loads(str(json_file.read()))
         return self.json_data
 
