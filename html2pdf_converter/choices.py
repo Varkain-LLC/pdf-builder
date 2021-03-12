0b3f16f087411bffef5e4999fd581437dff8d072
Evgenii Smirnov <knyazz@gmail.com>
2021-03-12 14:01:00 +0300

    fix names

--- html2pdf_converter/choices.py
+++ html2pdf_converter/choices.py
@@ -10,5 +10,5 @@ templates = {
     JRNL_TEMPLATE_ID: 'jrnl-template.html',
     JRNL_COVER_TEMPLATE_ID: 'jrnl-cover-template.html',
     FANBOOKS_TEMPLATE_ID: 'book1.html',
-    JRNL_FULL_COVER_TEMPLATE_ID: 'jrnl-full-cover.html'
+    JRNL_FULL_COVER_TEMPLATE_ID: 'jrnl-full-cover-6-9.html'
 }
